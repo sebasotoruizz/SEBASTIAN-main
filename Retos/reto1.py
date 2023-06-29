66b2500125e19e8e322e078ae00791b790690bf0
Sebastian Soto <129217207+sebasotoruizz@users.noreply.github.com>
2023-06-28 23:21:30 -0400

    Add files via upload

---
+++ Retos/reto1.py
@@ -0,0 +1,13 @@
+#Reto en clase n°1
+
+#"Hola mi nombre es [nombre], tengo [edad] años"
+
+#Inicio del reto
+
+nombre = input("¿Cuál es tu nombre?\n")
+edad = input("¿Cual es tu edad?\n")
+total = 20 + int(edad)
+
+print(f"Hola mi nombre es (nombre), tengo (edad) años y en 20 años tendré (total) años")
+
+#Fin del reto
